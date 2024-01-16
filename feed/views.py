a3ed6d1c2437ea2616b8b97d4ca28971f3a5c279
junbausa <juan_bausa@yahoo.com>
2024-01-16 23:23:55 +0800

    Adding custom view context

--- feed/views.py
+++ feed/views.py
@@ -7,3 +7,9 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["my_thing"] = "Hello world this is from context handler" 
+        return context
