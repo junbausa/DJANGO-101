49545ee479277cfce4021ef4e8f6094e33f5b309
junbausa <juan_bausa@yahoo.com>
2024-01-17 00:07:06 +0800

    Adding images to your model

--- feed/models.py
+++ feed/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from sorl.thumbnail import ImageField
 
 # Create your models here.
 class Post(models.Model):
     text = models.CharField(max_length=140, blank=False, null=False)
-
+    image = ImageField()
     # then make a migration
 
     # updating Data name in Database View Django
