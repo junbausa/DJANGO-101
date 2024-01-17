ae93185fe19341e82f9f085f529c69cfbe771110
junbausa <juan_bausa@yahoo.com>
2024-01-17 18:28:30 +0800

    Django messages framework

--- feed/views.py
+++ feed/views.py
@@ -8,6 +8,7 @@ from django.views.generic import TemplateView, DetailView, FormView
 
 from .models import Post
 from .forms import PostForm
+from django.contrib import messages
 
 class HomePageView(TemplateView):
 
@@ -18,7 +19,7 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         # context["my_thing"] = "Hello world this is from context handler" 
-
+        
         context["posts"] = Post.objects.all().order_by('-id')
         return context
 
@@ -35,6 +36,12 @@ class AddPostView(FormView):
     template_name = "new_post.html"
     form_class = PostForm # imported from forms.py contains fields in form
     success_url = "/"
+
+    # Getting the request object
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request # This is the code
+        return super().dispatch(request, *args, **kwargs)
+    
     
     def form_valid(self, form):
         
@@ -47,6 +54,9 @@ class AddPostView(FormView):
             text=form.cleaned_data['text'],
             image=form.cleaned_data['image']
         )
-        
+
+        # sample adding message usage via Django framework
+        messages.add_message(self.request, messages.SUCCESS, "Your post was successful")
+
         return super().form_valid(form)
     
